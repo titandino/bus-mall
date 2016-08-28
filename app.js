5d4083afa78d9b021adf76b947d8856fe99bf642
Trenton Kress <trenton.kress@gmail.com>
2016-08-28 11:50:56 -0700

    Lintered it up and added some more button functionality

--- app.js
+++ app.js
@@ -1,40 +1,73 @@
 function BMImage(name, fileName) {
   this.name = name;
-  this.path = 'img/'+fileName;
+  this.path = 'img/' + fileName;
   this.clicks = 0;
 }
+
+BMImage.prototype.click = function() {
+  this.clicks++;
+};
 
 var BusMall = {
   images: [],
   dispImage: [],
   totalClicks: 0,
 
+  click: function(event) {
+    console.log(event.target);
+    console.log(event.target.id);
+    console.log(parseInt(event.target.id));
+    console.log(this.dispImage);
+    this.dispImage[parseInt(event.target.id)].click();
+    this.totalClicks++;
+    this.randomize();
+  },
+
+  randomize: function() {
+    for (var i = 0;i < 3;i++) {
+      this.dispImage[i] = this.images[i];
+      console.log('Setting #' + i + ' to ' + this.dispImage[i]);
+    }
+    this.update();
+  },
+
+  update: function() {
+    for (var i = 0;i < 3;i++) {
+      document.getElementById('' + i).src = this.dispImage[i].path;
+    }
+  },
+
   loadImages: function() {
-    this.images.push(new BMImage('Bag', 'bag.png'));
-    this.images.push(new BMImage('Banana', 'banana.png'));
-    this.images.push(new BMImage('Bathroom', 'bathroom.png'));
-    this.images.push(new BMImage('Boots', 'boots.png'));
-    this.images.push(new BMImage('Breakfast', 'breakfast.png'));
-    this.images.push(new BMImage('Bubble Gum', 'bubblegum.png'));
-    this.images.push(new BMImage('Chair', 'chair.png'));
-    this.images.push(new BMImage('Cthulhu', 'cthulhu.png'));
-    this.images.push(new BMImage('Dog duck', 'dog-duck.png'));
-    this.images.push(new BMImage('Dragon', 'dragon.png'));
-    this.images.push(new BMImage('Pen', 'pen.png'));
-    this.images.push(new BMImage('Pet Sweep', 'pet-sweep.png'));
-    this.images.push(new BMImage('Scissors', 'scissors.png'));
-    this.images.push(new BMImage('Shark', 'shark.png'));
-    this.images.push(new BMImage('Sweep', 'sweep.png'));
-    this.images.push(new BMImage('Tauntaun', 'tauntaun.png'));
-    this.images.push(new BMImage('Unicorn', 'unicorn.png'));
-    this.images.push(new BMImage('USB', 'usb.png'));
-    this.images.push(new BMImage('Watering can', 'water-can.png'));
-    this.images.push(new BMImage('Wine glass', 'wine-glass.png'));
+    this.images.push(new BMImage('Bag', 'bag.jpg'));
+    this.images.push(new BMImage('Banana', 'banana.jpg'));
+    this.images.push(new BMImage('Bathroom', 'bathroom.jpg'));
+    this.images.push(new BMImage('Boots', 'boots.jpg'));
+    this.images.push(new BMImage('Breakfast', 'breakfast.jpg'));
+    this.images.push(new BMImage('Bubble Gum', 'bubblegum.jpg'));
+    this.images.push(new BMImage('Chair', 'chair.jpg'));
+    this.images.push(new BMImage('Cthulhu', 'cthulhu.jpg'));
+    this.images.push(new BMImage('Dog duck', 'dog-duck.jpg'));
+    this.images.push(new BMImage('Dragon', 'dragon.jpg'));
+    this.images.push(new BMImage('Pen', 'pen.jpg'));
+    this.images.push(new BMImage('Pet Sweep', 'pet-sweep.jpg'));
+    this.images.push(new BMImage('Scissors', 'scissors.jpg'));
+    this.images.push(new BMImage('Shark', 'shark.jpg'));
+    this.images.push(new BMImage('Sweep', 'sweep.jpg'));
+    this.images.push(new BMImage('Tauntaun', 'tauntaun.jpg'));
+    this.images.push(new BMImage('Unicorn', 'unicorn.jpg'));
+    this.images.push(new BMImage('USB', 'usb.gif'));
+    this.images.push(new BMImage('Watering can', 'water-can.jpg'));
+    this.images.push(new BMImage('Wine glass', 'wine-glass.jpg'));
   },
 
   main: function() {
     this.loadImages();
+    this.randomize();
   }
-}
+};
 
 BusMall.main();
+
+window.onload = function() {
+  document.getElementById('img-section').addEventListener('click', BusMall.click);
+};

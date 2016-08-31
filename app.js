05467b18519747ea30df79581820337a8b07bd81
Trenton Kress <trenton.kress@gmail.com>
2016-08-30 23:25:19 -0700

    Coloring and chart data finished

--- app.js
+++ app.js
@@ -52,25 +52,13 @@ var BusMall = {
       labels[i] = this.images[i].imgName;
       chartArgs.data.datasets[0].data[i] = this.images[i].clicks;
     }
+    chartArgs.data.datasets[0].backgroundColor = [];
+    chartArgs.data.datasets[0].borderColor = [];
+    for (var i = 0;i < this.images.length;i++) {
+      chartArgs.data.datasets[0].backgroundColor[i] = 'rgba(255, 99, 132, 0.2)';
+      chartArgs.data.datasets[0].borderColor[i] = 'rgba(255, 99, 132, 1)';
+    }
     new Chart(ctx, chartArgs);
-    /*
-    chartArgs.data.datasets[0].backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)'
-    ],
-    chartArgs.data.datasets[0].borderColor: [
-      'rgba(255,99,132,1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)'
-    ],
-    */
   },
 
   randomize: function() {
